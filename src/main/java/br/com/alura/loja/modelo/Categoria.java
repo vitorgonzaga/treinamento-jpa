ff7173870d58512ff6b3d4144c1d559e5d7d803f
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-09 11:51:38 -0300

    refactor(Categoria.java): transforming the Enum to Model Object.

--- src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,9 +1,21 @@
 package br.com.alura.loja.modelo;
 
-public enum Categoria {
+import javax.persistence.*;
 
-    CELULARES,
-    INFORMATICA,
-    ELETRODOMESTICOS
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
 
 }
