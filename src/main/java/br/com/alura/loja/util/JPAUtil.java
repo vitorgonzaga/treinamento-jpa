64ba7ba07c02c363c8be4748d20067f573d1de6b
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-11 10:45:24 -0300

    refactor(JPAUtil.java): increase the initial feed database

--- src/main/java/br/com/alura/loja/util/JPAUtil.java
+++ src/main/java/br/com/alura/loja/util/JPAUtil.java
@@ -1,8 +1,15 @@
 package br.com.alura.loja.util;
 
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ClienteDao;
+import br.com.alura.loja.dao.PedidoDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.*;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.math.BigDecimal;
 
 public class JPAUtil {
 
@@ -10,6 +17,50 @@ public class JPAUtil {
 
     public static EntityManager getEntityManager() {
         return FACTORY.createEntityManager();
+    }
+
+    public static void executaCadastroInicial() {
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+        Produto playstation = new Produto("PS5", "Playstation 5", new BigDecimal("1500"), videogames);
+        Produto macbook = new Produto("Macbook", "Mackboo pro retina", new BigDecimal("19000"), informatica);
+
+        Cliente cliente = new Cliente("Vitor Ferreira", "1234567-89");
+
+        Pedido pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(10, pedido, celular));
+        pedido.adicionarItem(new ItemPedido(40, pedido, playstation));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(2, pedido, macbook));
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        ClienteDao clienteDao = new ClienteDao(em);
+        PedidoDao pedidoDao = new PedidoDao(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
+        produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(playstation);
+        produtoDao.cadastrar(macbook);
+
+        clienteDao.cadastrar(cliente);
+
+        pedidoDao.cadastrar(pedido);
+        pedidoDao.cadastrar(pedido2);
+
+        em.getTransaction().commit(); // indispensável para a persistência no banco de dados
+
+        em.close();
     }
 
 }
